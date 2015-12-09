cae1d8ae3a9b8fb13ecbd7e106580342fda88dda
m-vdb <mvergerdelbove@work4labs.com>
2015-12-09 14:34:16 -0800

    remove trailing spaces

--- xplenty/xplenty_api.py
+++ xplenty/xplenty_api.py
@@ -61,7 +61,7 @@ def to_python(obj,
         for in_key in int_keys:
             if (in_dict is not None) and (in_dict.get(in_key) is not None):
                 d[in_key] = int(in_dict.get(in_key))
-    
+
     if float_keys:
         for in_key in float_keys:
             if (in_dict is not None) and (in_dict.get(in_key) is not None):
@@ -167,7 +167,7 @@ class BaseModel(object):
         d.__dict__.update(kwargs)
 
         return d
-    
+
 
 class Cluster(BaseModel):
     """Xplenty Cluster."""
@@ -180,8 +180,8 @@ class Cluster(BaseModel):
 
     def __repr__(self):
         return "<Cluster '{0}'>".format(self.name)
-    
-    
+
+
 
 class Job(BaseModel):
     """Xplenty Job."""
@@ -195,14 +195,14 @@ class Job(BaseModel):
 
     def __repr__(self):
         return "<Job '{0}'>".format(self.name)
-    
-    
+
+
 class AccountLimits(BaseModel):
     """Xplenty Account limits."""
 
-   
+
     _ints = ['limit','remaining']
-    
+
 
     def __repr__(self):
         return "<AccountLimits '{0}'>".format(self.name)
@@ -234,38 +234,38 @@ class RequestWithMethod(urllib2.Request):
         if self._method:
             return self._method
         else:
-            return urllib2.Request.get_method(self) 
+            return urllib2.Request.get_method(self)
 
 class XplentyClient(object):
-    
+
     version = "1.0"
     def __init__(self, account_id="", api_key=""):
         self.account_id = account_id
         self.api_key = api_key
-        
-    
+
+
     def __repr__(self):
         return '<Xplenty client at 0x%x>' % (id(self))
-    
-    
+
+
     def get(self,url):
         logger.debug("GET %s", url)
         request = urllib2.Request(url,headers=HEADERS)
         base64string = base64.encodestring('%s' % (self.api_key)).replace('\n', '')
         request.add_header("Authorization", "Basic %s" % base64string)
-        
+
         contents =""
         try:
             resp = urllib2.urlopen(request)
             contents = resp.read()
-           
+
         except urllib2.HTTPError, error:
             print error
             print error.read()
             contents =""
-        
+
         return json.loads(contents)
-    
+
     def post(self, url, data_dict={}):
         encoded_data = urllib.urlencode(data_dict)
         logger.debug("POST %s, data %s", url, encoded_data)
@@ -283,7 +283,7 @@ class XplentyClient(object):
             contents =""
 
         return json.loads(contents)
-    
+
     def delete(self, url):
         logger.debug("DELETE %s", url)
         request = RequestWithMethod(url, 'DELETE', headers=HEADERS)
@@ -301,37 +301,37 @@ class XplentyClient(object):
             contents =""
 
         return json.loads(contents)
-    
+
     def _join_url(self, method):
         _url = API_URL % ( self.account_id )
         url = urljoin(_url , method )
         return url
-    
+
     def get_clusters(self):
         method_path = 'clusters'
         url = self._join_url( method_path )
         resp =self.get(url)
         clusters =  [Cluster.new_from_dict(item, h=self) for item in resp]
-        
+
         return clusters
-    
+
     def get_cluster(self,id):
         method_path = 'clusters/%s'%(str(id))
         url = self._join_url( method_path )
         resp =self.get(url)
         cluster =  Cluster.new_from_dict(resp, h=self)
-        
+
         return cluster
-    
+
     def terminate_cluster(self,id):
         method_path = 'clusters/%s'%(str(id))
         url = self._join_url( method_path )
         resp =self.delete(url)
         cluster =  Cluster.new_from_dict(resp, h=self)
-       
+
         return cluster
-    
-    
+
+
     def create_cluster(self, cluster_type, nodes, cluster_name, cluster_description, terminate_on_idle=False, time_to_idle=3600):
         cluster_info ={}
         cluster_info["cluster[type]"]= cluster_type
@@ -344,66 +344,66 @@ class XplentyClient(object):
         url = self._join_url( method_path )
         resp =self.post(url,cluster_info)
         cluster =  Cluster.new_from_dict(resp, h=self)
-        
+
         return cluster
-    
-    
-    
+
+
+
     def get_jobs(self):
-        
+
         method_path = 'jobs'
         url = self._join_url(method_path )
         resp =self.get(url)
-        
+
         jobs =  [Job.new_from_dict(item, h=self) for item in resp]
-        
+
         return jobs
-    
+
     def get_job(self,id):
         method_path = 'jobs/%s'%(str(id))
         url = self._join_url( method_path )
         resp =self.get(url)
         job =  Job.new_from_dict(resp, h=self)
-        
+
         return job
-    
+
     def stop_job(self,id):
         method_path = 'jobs/%s'%(str(id))
         url = self._join_url( method_path )
         resp =self.delete(url)
-       
+
         return resp
-    
-    
+
+
     def add_job(self, cluster_id, package_id, vars={}):
-        
+
         job_info ={}
         job_info["job[cluster_id]"]= cluster_id
 				# We use job_id instead of package_id since that's how it is accepted on Xplenty's side
         job_info["job[job_id]"]= package_id
-        
+
         for k, v in vars.iteritems():
             new_key = "job[variables][%s]"%(k)
             job_info[new_key]= v
-            
+
         method_path = 'jobs'
         url = self._join_url( method_path )
         resp =self.post(url,job_info)
         job =  Job.new_from_dict(resp, h=self)
-        
+
         return job
-    
-    
+
+
     def get_account_limits(self):
-        
+
         method_path = 'rate_limit_status'
         url = self._join_url( method_path )
         resp =self.get(url)
-        
-        limit =  AccountLimits.new_from_dict(resp['limits'], h=self) 
-        
+
+        limit =  AccountLimits.new_from_dict(resp['limits'], h=self)
+
         return limit
-    
+
     def get_packages(self):
         method_path = 'packages'
         url = self._join_url(method_path)
@@ -411,7 +411,7 @@ class XplentyClient(object):
         packages = [Package.new_from_dict(item, h=self) for item in resp]
 
         return packages
-    
+
     def get_package(self, id):
         method_path = 'packages/%s' % id
         url = self._join_url(method_path)
@@ -419,19 +419,19 @@ class XplentyClient(object):
         package = Package.new_from_dict(resp, h=self)
 
         return package
-     
+
     @property
     def clusters(self):
         return self.get_clusters()
-    
+
     @property
     def jobs(self):
         return self.get_jobs()
-    
+
     @property
     def account_limits(self):
         return self.get_account_limits()
-  
+
     @property
     def packages(self):
         return self.get_packages()
