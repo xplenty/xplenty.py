52be3cc7de9cfae679f6788d9d910d103146ba92
Peter Wicks <pwicks@apartmentlist.com>
2023-01-30 12:29:04 -0700

    Changing version to 3.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_project_path(*args):
 
 
 PACKAGE_NAME = 'xplenty'
-PACKAGE_VERSION = '3.2.0'
+PACKAGE_VERSION = '3.2.1'
 PACKAGE_AUTHOR = 'Xplenty'
 PACKAGE_AUTHOR_EMAIL = 'opensource@xplenty.com'
 PACKAGE_URL = 'https://github.com/xplenty/xplenty.py'
